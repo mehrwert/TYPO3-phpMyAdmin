5669f93f0fac0e3d63268d55bf791c685b48bff1
Andreas Beutel <Andreas.Beutel@mehrwert.de>
2015-09-29 15:50:12 +0200

    [FEATURE] Update pMA to the latest stable 4.4.x release (4.4.15) and set TYPO3 7.5 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Third party \'phpMyAdmin\' administration module. Access to admin-users only.',
 	'category' => 'module',
 	'shy' => 0,
-	'version' => '5.1.1',
+	'version' => '5.1.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-5.6.99',
-			'typo3' => '6.2.0-7.4.99',
+			'typo3' => '6.2.0-7.5.99',
 		),
 		'conflicts' => array(
 		),
